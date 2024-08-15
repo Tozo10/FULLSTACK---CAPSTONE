6aa95e6314bc50bb189cd561136f44b26eae3098
Tozo10 <sayantanchatterjee00@gmail.com>
2024-08-15 10:53:09 -0400

    Linted Python Codes

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,34 +4,38 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 class CarMakeAdmin(admin.ModelAdmin):
-    list_display = ('name', 'country_of_origin', 'established_date')
-    search_fields = ('name', 'country_of_origin')
-    list_filter = ('country_of_origin',)
-    
+    list_display = ("name", "country_of_origin", "established_date")
+    search_fields = ("name", "country_of_origin")
+    list_filter = ("country_of_origin",)
+
     fieldsets = (
-        (None, {
-            'fields': ('name', 'description')
-        }),
-        ('Additional Information', {
-            'fields': ('country_of_origin', 'established_date', 'logo', 'website'),
-            'classes': ('collapse',)
-        }),
+        (None, {"fields": ("name", "description")}),
+        (
+            "Additional Information",
+            {
+                "fields": ("country_of_origin", "established_date", "logo", "website"),
+                "classes": ("collapse",),
+            },
+        ),
     )
 
+
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_make', 'type', 'year', 'is_electric')
-    search_fields = ('name', 'car_make__name', 'type')
-    list_filter = ('type', 'year', 'is_electric')
-    
+    list_display = ("name", "car_make", "type", "year", "is_electric")
+    search_fields = ("name", "car_make__name", "type")
+    list_filter = ("type", "year", "is_electric")
+
     fieldsets = (
-        (None, {
-            'fields': ('car_make', 'name', 'type', 'year')
-        }),
-        ('Additional Information', {
-            'fields': ('description', 'price', 'is_electric', 'photo'),
-            'classes': ('collapse',)
-        }),
+        (None, {"fields": ("car_make", "name", "type", "year")}),
+        (
+            "Additional Information",
+            {
+                "fields": ("description", "price", "is_electric", "photo"),
+                "classes": ("collapse",),
+            },
+        ),
     )
+
 
 # Registering models with their respective admins
 admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -7,26 +7,28 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
+
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     country_of_origin = models.CharField(max_length=100, blank=True, null=True)
     established_date = models.DateField(blank=True, null=True)
-    logo = models.ImageField(upload_to='car_makes/logos/', blank=True, null=True)
+    logo = models.ImageField(upload_to="car_makes/logos/", blank=True, null=True)
     website = models.URLField(blank=True, null=True)
-    
+
     class Meta:
         verbose_name = "Car Make"
         verbose_name_plural = "Car Makes"
-        ordering = ['name']  # Orders the car makes by name alphabetically
-    
+        ordering = ["name"]  # Orders the car makes by name alphabetically
+
     def __str__(self):
         return self.name  # Return the name as the string representation
-    
+
     def get_established_year(self):
         if self.established_date:
             return self.established_date.year
         return None
+
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many
@@ -38,36 +40,34 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        ('COUPE', 'Coupe'),
-        ('CONVERTIBLE', 'Convertible'),
-        ('HATCHBACK', 'Hatchback'),
-        ('TRUCK', 'Truck'),
-        ('VAN', 'Van'),
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        ("COUPE", "Coupe"),
+        ("CONVERTIBLE", "Convertible"),
+        ("HATCHBACK", "Hatchback"),
+        ("TRUCK", "Truck"),
+        ("VAN", "Van"),
         # Add more choices as required
     ]
-    type = models.CharField(max_length=20, choices=CAR_TYPES, default='SUV')
+    type = models.CharField(max_length=20, choices=CAR_TYPES, default="SUV")
     year = models.IntegerField(
-        default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ]
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
     )
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     is_electric = models.BooleanField(default=False)
-    photo = models.ImageField(upload_to='car_models/photos/', blank=True, null=True)
-    
+    photo = models.ImageField(upload_to="car_models/photos/", blank=True, null=True)
+
     class Meta:
         verbose_name = "Car Model"
         verbose_name_plural = "Car Models"
-        ordering = ['name']  # Orders the car models by name alphabetically
-    
+        ordering = ["name"]  # Orders the car models by name alphabetically
+
     def __str__(self):
         return f"{self.name} ({self.year})"  # Return the name and year as the string representation
